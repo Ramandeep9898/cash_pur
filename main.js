d262c63e577bf14a4b4edfdf215bd54aa4c86d89
Ramandeep9898 <ramank6238299@gmail.com>
2021-11-21 17:09:25 +0530

    upgrade

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
+
 var initialPrice = document.querySelector("#initial-price");
 var stocksQuantity = document.querySelector("#stocks-quantity");
 var currentPrice = document.querySelector("#current-price");
 var submitBtn = document.querySelector("#submit-btn");
 var outputBox = document.querySelector("#output-box");
-// console.log(intialPrice);
+
 submitBtn.addEventListener("click", submitHandler);
 
 function submitHandler() {
@@ -18,18 +19,22 @@ function calculateProfitAndLoss(initial, quantity, current) {
   if (initial > current) {
     var loss = (initial - current) * quantity;
     var lossPercentage = (loss / initial) * 100;
-    showOutput("Hey the loss is ${loss} and the percent is ${lossPercentage}%");
+
+    showOutput(
+      `Hey, the loss is ${loss} and the percent is ${lossPercentage}%`
+    );
   } else if (current > initial) {
     var profit = (current - initial) * quantity;
     var profitPercentage = (profit / initial) * 100;
 
     showOutput(
-      "hey the profit is ${profit} and the percent is ${profitPercentage}% "
+      `Hey, the profit is ${profit} and the percent is ${profitPercentage}%`
     );
   } else {
-    showOutput("No pain no gain and no gain no pain");
+    showOutput(`No pain no gain and no gain no pain`);
   }
 }
+
 function showOutput(message) {
   outputBox.innerHTML = message;
 }
